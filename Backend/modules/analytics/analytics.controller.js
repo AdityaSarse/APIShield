d1d72eb2ff1e2c17fc844db5020573ee90603c0a
Aditya Sarse <adityasarse362@gmail.com>
2026-08-11 11:17:42 +0530

    feat(analytics): add status code analytics

--- Backend/modules/analytics/analytics.controller.js
+++ Backend/modules/analytics/analytics.controller.js
@@ -1,5 +1,8 @@
 import ApiResponse from "../../utils/ApiResponse.js";
-import { getAnalyticsSummary } from "./analytics.service.js";
+import {
+  getAnalyticsSummary,
+  getStatusCodeAnalytics,
+} from "./analytics.service.js";
 
 export const analyticsSummary = async (req, res, next) => {
   try {
@@ -10,6 +13,22 @@ export const analyticsSummary = async (req, res, next) => {
         200,
         "Analytics summary fetched successfully",
         summary
+      )
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const statusCodeAnalytics = async (req, res, next) => {
+  try {
+    const analytics = await getStatusCodeAnalytics();
+
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        "Status code analytics fetched successfully",
+        analytics
       )
     );
   } catch (error) {
